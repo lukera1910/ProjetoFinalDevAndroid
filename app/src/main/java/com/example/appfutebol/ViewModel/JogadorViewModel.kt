7557cebc904b556ee0116e740cc9f33544fb2460
NevaxN <arthurmayersantos@gmail.com>
2024-11-20 11:28:31 -0300

    Mudando classe para utilizar funções DAO

--- app/src/main/java/com/example/appfutebol/ViewModel/JogadorViewModel.kt
+++ app/src/main/java/com/example/appfutebol/ViewModel/JogadorViewModel.kt
@@ -1,35 +1,130 @@
 package com.example.appfutebol.ViewModel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.appfutebol.Dao.JogadorDao
 import com.example.appfutebol.Model.Jogador
+import kotlinx.coroutines.launch
 
-class JogadorViewModel : ViewModel() {
+class JogadorViewModel(private val jogadorDao: JogadorDao) : ViewModel() {
+
+    var listaJogadores = mutableStateOf(listOf<Jogador>())
+        private set
+
+    init {
+        carregarJogadores()
+    }
+
+    //Carrega todos os Jogadores do Banco
+    private fun carregarJogadores(){
+        viewModelScope.launch {
+            listaJogadores.value = jogadorDao.buscarTodosJogadores()
+        }
+    }
+
+    //Faz a Busca no banco do jogador com esse Id, se não existir retorna null
+    //Função Assíncrona
+    suspend fun buscarJogadorPorId(id: Int): Jogador? {
+        return jogadorDao.buscarJogadorPorId(id)
+    }
+
+    //Faz a Busca no banco do jogador com esse Nome, se não existir retorna null
+    //Função Assíncrona
+    suspend fun buscarJogadorPorNome(nome: String): Jogador? {
+        return jogadorDao.buscarJogadorPorNome(nome)
+    }
+
+    //Faz a Busca no banco do jogador com essa Posição, se não existir retorna null
+    //Função Assíncrona
+    suspend fun buscarJogadorPorPosicao(posicao: String): Jogador? {
+        return jogadorDao.buscarJogadorPorPosicao(posicao)
+    }
+
+    //Salva o Novo Jogador no Banco
+    suspend fun salvarJogador(nome: String, posicao: String, numero: Int, pernaBoa: String, altura: Double,
+                      idade: Int, nacionalidade: String): String{
+        if(nome.isBlank() || posicao.isBlank()|| pernaBoa.isBlank() || altura <= 0 || nacionalidade.isBlank() ){
+            return "Preencha todos os campos!"
+        }
+
+        val jogador = Jogador(id = 0, nome, posicao, numero, pernaBoa, altura, idade, nacionalidade)
+
+        viewModelScope.launch {
+            jogadorDao.inserirJogador(jogador)
+            carregarJogadores()
+        }
+
+        return "Jogador salvo com sucesso!"
+    }
+
+    //Atualiza um Jogador
+    suspend fun atualizarJogador(id: Int, nome: String, posicao: String, numero: Int, pernaBoa: String,
+                                 altura: Double, idade: Int, nacionalidade: String): String {
+        if(nome.isBlank() || posicao.isBlank()|| pernaBoa.isBlank() || altura <= 0 || nacionalidade.isBlank() ) {
+            return "Preencha todos os campos!"
+        }
+
+
+        val jogador = buscarJogadorPorId(id) ?: return "Jogador com ID $id não encontrado!"
+
+        val jogadorAtualizado = jogador.copy(
+            nome = nome,
+            posicao = posicao,
+            numero = numero,
+            pernaBoa = pernaBoa,
+            altura = altura,
+            idade = idade,
+            nacionalidade = nacionalidade
+        )
+
+        viewModelScope.launch {
+            jogadorDao.editarJogador(jogadorAtualizado)
+            carregarJogadores()
+        }
+
+        return "Jogador Atualizado!"
+    }
+
+    //Exclui um jogador
+    suspend fun excluirJogador(jogador: Jogador): String{
+        return try{
+            viewModelScope.launch {
+                jogadorDao.excluirJogador(jogador)
+                carregarJogadores()
+            }
+
+            "Jogador excluído com sucesso!"
+        } catch (e: Exception){
+            "Erro ao excluir jogador: ${e.message}"
+        }
+    }
 
     // MutableLiveData para armazenar a lista de jogadores
-    private val _todosJogadores = MutableLiveData<List<Jogador>>()
-    val todosJogadores: LiveData<List<Jogador>> get() = _todosJogadores
-
-    // Função para adicionar jogadores à lista
-    fun adicionarJogador(jogador: Jogador) {
-        val currentList = _todosJogadores.value?.toMutableList() ?: mutableListOf()
-        currentList.add(jogador)
-        _todosJogadores.value = currentList
-    }
-
-    // Função para carregar jogadores iniciais
-    fun carregarJogadores() {
-        val jogadoresIniciais = listOf(
-            Jogador(
-                nome = "Cristiano Ronaldo", posicao = "Atacante", numero = 7,
-                pernaBoa = "Direita", altura = 1.87, idade = 39, nacionalidade = "Português"
-            ),
-            Jogador(
-                nome = "Lionel Messi", posicao = "Meio-campo", numero = 10,
-                pernaBoa = "Esquerda", altura = 1.70, idade = 36, nacionalidade = "Argentino"
-            )
-        )
-        _todosJogadores.value = jogadoresIniciais
-    }
+//    private val _todosJogadores = MutableLiveData<List<Jogador>>()
+//    val todosJogadores: LiveData<List<Jogador>> get() = _todosJogadores
+//
+//    // Função para adicionar jogadores à lista
+//    fun adicionarJogador(jogador: Jogador) {
+//        val currentList = _todosJogadores.value?.toMutableList() ?: mutableListOf()
+//        currentList.add(jogador)
+//        _todosJogadores.value = currentList
+//    }
+//
+//    // Função para carregar jogadores iniciais
+//    fun carregarJogadores() {
+//        val jogadoresIniciais = listOf(
+//            Jogador(
+//                nome = "Cristiano Ronaldo", posicao = "Atacante", numero = 7,
+//                pernaBoa = "Direita", altura = 1.87, idade = 39, nacionalidade = "Português"
+//            ),
+//            Jogador(
+//                nome = "Lionel Messi", posicao = "Meio-campo", numero = 10,
+//                pernaBoa = "Esquerda", altura = 1.70, idade = 36, nacionalidade = "Argentino"
+//            )
+//        )
+//        _todosJogadores.value = jogadoresIniciais
+//    }
 }
